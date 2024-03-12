2ea9325fb0a2a77cb999ae9115cc16410e3f8fe0
Romeo Daniel <romeo.butica@gmail.com>
2024-03-12 11:57:22 +0200

    feat: [jira-0001] - updated phone regex & the homepage style, small updated for README

--- src/utils/validations.tsx
+++ src/utils/validations.tsx
@@ -2,8 +2,20 @@ const usernamePattern = new RegExp(
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#])[a-zA-Z0-9!@#]{4,80}$/
 );
 const namePattern = new RegExp(/^[a-zA-Z\']{2,80}$/);
+/*
+ * phone regex with country code, exemples of valid phone numbers:
+ * (123) 456-7890
+ * (123)456-7890
+ * 123.456.7890
+ * 1234567890
+ * +34123456789
+ * +10123456789
+ * +40721234567
+ * 072-2345782
+ * 0721234567
+ */
 const phonePattern = new RegExp(
-  /^[+]{1}(?:[0-9\-\\(\\)\\/.]\s?){6,15}[0-9]{1}$/
+  /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/i
 );
 
 const validateUsername = (username: string) => usernamePattern.test(username);
